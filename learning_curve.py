838224e60d06bb04e5ac00354330f39e840674d1
kevinkang <1361788628@qq.com>
2024-12-03 00:32:16 +0800

    house keeping

--- learning_curve.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-reward = pd.read_csv("monitor.csv")
-print(reward)
-plt.plot(reward[" r "])
-plt.show()
--- old_data/conti_lastday/other material/VDI/RL truck/RL-truck/Math                                     rewarddeviation=(ω1∗L1+ω2∗L2)∗v.md
+++
@@ -1,4 +0,0 @@
-$$
-reward_{deviation}=(\omega_1*L_1+\omega_2*L_2)*v
-$$
-
